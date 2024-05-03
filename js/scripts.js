cd340fcc0507a6a6c5e4d6ab4984cd57f5824d3c
Phillweston <2436559745@qq.com>
2024-05-03 23:00:02 +0800

    Add address display container

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,29 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+function copyAddress(event) {
+    event.preventDefault();
+
+    var copyInput = document.getElementById("address");
+    var fullAddress = copyInput.getAttribute("data-full-address");
+
+    // Using the Clipboard API
+    navigator.clipboard.writeText(fullAddress).then(function() {
+        console.log('Address copied to clipboard successfully!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+
+    // Create and show a hint message
+    var hint = document.createElement('span');
+    hint.innerText = 'Copied to clipboard';
+    hint.style.cssText = 'position: absolute; color: green; background-color: white; padding: 5px; border-radius: 5px; margin-left: 10px;';
+    var button = document.querySelector('.address-container button');
+    button.parentNode.insertBefore(hint, button.nextSibling);
+
+    // Remove the hint after 2 seconds
+    setTimeout(function() {
+        hint.remove();
+    }, 2000);
+}
